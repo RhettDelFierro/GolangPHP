52a8357bd47b67b528c476284e538cd50f57a527
Rhett Del Fierro <thinquetank@gmail.com>
2016-05-03 18:23:36 -0700

    hard coded looping added

--- src/main/main.go
+++ src/main/main.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"fmt"
 )
 
 type Students struct{
 	Name 	string
 	Course 	string
 	Grade 	int
+}
+
+type School struct{
+	All []Students
 }
 
 var tmpl *template.Template
@@ -26,9 +31,13 @@ func init() {
 
 //HandlerFunc
 func inject(w http.ResponseWriter, req *http.Request){
-	student1 := Students{Name: "Student1", Course: "Course1", Grade: 100}
+	student1 := Students{Name: "Student1", Course: "Course1", Grade: 100,}
+	student2 := Students{Name: "Student2", Course: "Course2", Grade: 100,}
+	student3 := Students{Name: "Student1", Course: "Course3", Grade: 100,}
+	class := School{All: []Students{student1, student2, student3}}
+	fmt.Println(class)
 	//Execute goes here.
-	tmpl.Execute(w, student1)
+	tmpl.Execute(w, class)
 }
 
 func main() {

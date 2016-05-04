ff139d4c426bf9cf70b3ce5e0592c08892c5294d
Rhett Del Fierro <thinquetank@gmail.com>
2016-05-03 17:36:20 -0700

    added right directory to fs

--- src/main/main.go
+++ src/main/main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"net/http"
+	"html/template"
+	"log"
 )
 
-func main(){
+type Students struct{
+	Name 	string
+	Course 	string
+	Grade 	int
+}
+
+var tmpl *template.Template
+
+//parse the html template file
+func init() {
+	var err error
+	tmpl, err = template.ParseFiles("../../templates/index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+//HandlerFunc
+func inject(w http.ResponseWriter, req *http.Request){
+	student1 := Students{Name: "Student1", Course: "Course1", Grade: 100}
+	//Execute goes here.
+	tmpl.Execute(w, student1)
+}
+
+func main() {
 	mux := http.NewServeMux()
-	fs :=http.FileServer(http.Dir("templates"))
+	fs := http.FileServer(http.Dir("../../templates"))
 	mux.Handle("/", fs)
 	http.ListenAndServe(":8080", mux)
-
 }

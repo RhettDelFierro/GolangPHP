4a9da70c8548ac667a5789e8e460a81e9583ba7a
Rhett Del Fierro <thinquetank@gmail.com>
2016-05-05 16:07:59 -0700

    tried parameterized routing without gorillamux, no dide

--- src/controllers/table.go
+++ src/controllers/table.go
@@ -4,6 +4,9 @@ import(
 	"net/http"
 	"github.com/RhettDelFierro/GolangPHP/src/viewmodels"
 	"text/template"
+	"strconv"
+	"log"
+	"fmt"
 )
 
 type gradesController struct {
@@ -11,7 +14,12 @@ type gradesController struct {
 }
 
 func (this *gradesController) get(w http.ResponseWriter, req *http.Request){
-	vm := viewmodels.GetGrades()
+	id, err := strconv.Atoi(req.URL.Path[1:])
+	fmt.Println(id)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	vm := viewmodels.GetGrades(id)
 
 	w.Header().Add("Content Type", "text/html")
 
--- src/viewmodels/table.go
+++ src/viewmodels/table.go
@@ -1,24 +1,41 @@
 package viewmodels
 
-import(
+import ()
 
-)
-
-type Students struct{
-	Name 	string
-	Course 	string
-	Grade 	int
+type Students struct {
+	Name   string
+	Course string
+	Grade  int
+	Id     int
 }
 
-type School struct{
+type School struct {
 	All []Students
 }
 
-func GetGrades() School {
-	student1 := Students{Name: "Student1", Course: "Course1", Grade: 100,}
-	student2 := Students{Name: "Student2", Course: "Course2", Grade: 100,}
-	student3 := Students{Name: "Student1", Course: "Course3", Grade: 100,}
-	class := School{All: []Students{student1, student2, student3}}
+func GetGrades(id int) Students {
+	var student Students
 
-	return class
+	student1 := Students{Name: "Student1", Course: "Course1", Grade: 100, Id: 1}
+	student2 := Students{Name: "Student2", Course: "Course2", Grade: 100, Id: 2}
+	student3 := Students{Name: "Student3", Course: "Course3", Grade: 100, Id: 3}
+
+	switch id {
+		case 1:
+			student = student1
+
+		case 2:
+			student = student2
+
+		case 3:
+			student = student3
+	}
+
+
+	//class := School{All: []Students{student1, student2, student3}}
+
+	//return class
+	//return class.All[id-1]
+	return student
+	//figure out a way to return the whole school when they want to populate it (maybe make that an option on sgt html)
 }

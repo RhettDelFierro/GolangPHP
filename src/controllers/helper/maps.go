e2ca9d59d9f7e2fb01db1b1f78036bcb04ff1205
Rhett Del Fierro <thinquetank@gmail.com>
2016-05-09 17:04:26 -0700

    need to change the TestvalidEntry to be called in the controller

--- src/controllers/helper/maps.go
+++ src/controllers/helper/maps.go
@@ -48,32 +48,46 @@ func (this *NewStudent) Make(name string,course string, grade string){ //pass in
 
 func (this* NewStudent) ErrorMaker(postData map[string]string) {
 
-	//safeEntry is now a struct. But no regex texts yet. Injection has been prevented.
-	var safeEntry = Convert(postData);
-	//the regex tests will determine whether the addedstudent's info is an acceptable pattern.
-	TestValidEntry()
+	type studentStruct struct {
+		name string
+		course string
+		grade int
+	}
 
+	//safeEntry is now a struct. But no regex texts yet. Injection has been prevented.
+	var safeEntry studentStruct
+	safeEntry = Convert(postData);
+
+	catchRegexArray := TestValidEntry(safeEntry)
+
+	//the regex tests will determine whether the addedstudent's info is an acceptable pattern.
+	if (len(catchRegexArray) == 0){
+		//do the code at the bottom:
+	} else {
+		//match the name of the element of the current array and return it with the "invalid" key/value.
+	}
+	//or you can have this whole method just punch in the safeEntry values and TestValidEntry is called in the controller.
 	this.Name = map[string]string{
-		"value": safeName.Name,
+		"value": safeEntry.name,
 		"invalid": "Invalid name, please use only letters and numbers",
 		"error": "There was an error adding student to database.",
 	}
 	this.Course = map[string]string{
-		"value": course,
+		"value": safeEntry.course,
 		"invalid": "Only numbers and letters please.",
 		"error": "There was an error adding student to database",
 	}
 	this.Grade = map[string]interface{}{
-		"value": grade,
+		"value": safeEntry.grade,
 		"invalid": "Only numbers please",
 		"error": "There was an error adding student to database",
 	}
 	this.Duplicate = map[string]interface{}{
-		"value": "",
+		"value": "",//struct{},
 		"error": "Records show you've alread recorded this entry", //continue to add (if they're the same user that added the name in the first place)?
 	}
 	this.Auth_token = map[string]string{
-		"value":	auth_token,
+		"value":	"",
 		"access_error":	"you do not have the privileges to add a student",
 		"expired":	"Login session expired",
 	}
--- src/controllers/helper/regex.go
+++ src/controllers/helper/regex.go
@@ -4,18 +4,21 @@ import (
 	"regexp"
 )
 
-func TestValidEntry(entry struct{}) bool {
-	regex_tests = make(map[string]string)
+func TestValidEntry(entry struct{}) []string {
+	regex_tests := make(map[string]string)
 	//32 alphanumeric characters. No spaces, but underscores allowed
 	regex_tests["name"] = "/^[A-Za-z0-9_]{1,32}$/"
 	regex_tests["course"] = "/^[A-Za-z0-9_]{1,32}$/"
 	//only numbers
 	regex_tests["grade"] = "/^100$|^[1-9]?[0-9]$/"
 
-	for entry,field := range entry {
-		if(regexp.Match(regex_tests[entry]))
+	var regex_array []string
+	for name,_ := range entry {
+		if(regexp.Match(regex_tests[name], name)){
+			regex_array = append(regex_array, name)
+		}
 	}
 
-	return true
+	return regex_array
 	//return preg_match($regex_tests[$key], $value);
 }
